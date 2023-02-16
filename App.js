050fb9b2adfd3e4969ad807b2eca1e2f19247fdc
Sai1916 <saisumedh16@gmail.com>
2023-02-16 12:44:32 +0530

    app

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {useTailwind} from 'tailwind-rn'
 
 export default function App() {
+
+  const tailwind = useTailwind();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={tailwind('flex-1 items-center bg-blue-400')}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
   );
 }
 
